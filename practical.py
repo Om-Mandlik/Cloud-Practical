6f15f76975b80839d1af13b37a508b052e432bde
Om Mandlik <ommandlik702@gmail.com>
2026-05-15 12:13:44 +0530

    Update practical.py

--- practical.py
+++ practical.py
@@ -1,10 +1,10 @@
+print("\n" * 2)
+print("====================================")
+print("        🤖 PY CHAT BOT APP        ")
+print("====================================")
+    
 while True:
-    print("\n" * 2)
-    print("====================================")
-    print("        🤖 PY CHAT BOT APP        ")
-    print("====================================")
     msg = input("You: ").lower()
-
     if "hello" in msg:
         print("Bot: Hi there!")
     elif "how are you" in msg:
