4ba7a8150aa4e9e79868d6e8f22270369d4c0cd3
Om Mandlik <ommandlik702@gmail.com>
2026-05-15 12:17:57 +0530

    Implement current time response in chat bot
    Added functionality to display the current time when prompted.

--- practical.py
+++ practical.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 print("\n" * 2)
 print("====================================")
 print("        🤖 PY CHAT BOT APP        ")
@@ -8,7 +10,11 @@ while True:
     if "hello" in msg:
         print("Bot: Hi there!")
     elif "how are you" in msg:
-        print("Bot: I'm just code, but I'm doing fine 😄")
+        print("Bot: I'm just code, but I'm doing fine ")
+    elif "what time " in msg: 
+        current_time = datetime.now().strftime("%H:%M:%S")
+        print("Current Time:", current_time)
+    
     elif "bye" in msg:
         print("Bot: Goodbye!")
         break
