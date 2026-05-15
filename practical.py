a7cfc376213d9079775a46739069127deaaa2176
Om Mandlik <ommandlik702@gmail.com>
2026-05-15 12:07:58 +0530

    Replace even/odd check with a simple chatbot

--- practical.py
+++ practical.py
@@ -1,5 +1,12 @@
-n = int(input("Enter a number to check is it even or odd"))
-if (n % 2 == 0):
-    print("The number",n,"is even")
-else:
-    print("The number ",n,"is odd")
+while True:
+    msg = input("You: ").lower()
+
+    if "hello" in msg:
+        print("Bot: Hi there!")
+    elif "how are you" in msg:
+        print("Bot: I'm just code, but I'm doing fine 😄")
+    elif "bye" in msg:
+        print("Bot: Goodbye!")
+        break
+    else:
+        print("Bot: I don't understand that.")
